f2ec89be26c1b697c8718b386227191903ac8016
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-06-03 13:29:32 +0200

    Added point list serving

--- api/api.php
+++ api/api.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Headers: Content-Type, X-Session-Id');
 
 $resource = null;
 if(isset($_GET['resource']))
@@ -55,6 +57,40 @@ if($resData[0] == 'MapTile') {
 
     echo(json_encode($tile));
     return;
+}
+
+if($resData[0] == 'PointList') {
+    $db = new mysqli('localhost', 'root', '', 'photopos');
+
+    if(!isset($_SERVER['HTTP_X_SESSION_ID'])){
+        echo '{"success": false, "error": "No session ID"}';
+        exit;
+    }
+
+    $sessionId = $_SERVER['HTTP_X_SESSION_ID'];
+    $result = $db->query('SELECT * FROM sessions WHERE id = "'.$sessionId.'" LIMIT 1');
+    if($result->num_rows == 0){
+        echo '{"success": false, "error": "Invalid session ID"}';
+        exit;
+    }
+    $user_id = $result->fetch_assoc()['user_id'];
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $requestContent = file_get_contents('php://input');
+        $requestContent = addslashes($requestContent);
+
+        $db->query('REPLACE INTO lists (user_id, list_data) VALUES ('.$user_id.', "'.$requestContent.'")');
+        return;
+    }else{
+        $result = $db->query('SELECT * FROM lists WHERE user_id = '.$user_id.' LIMIT 1');
+        if($result->num_rows == 0){
+            echo '{"success": true, "payload": {"type": "PointList", "data": []}}';
+            return;
+        }
+        $row = $result->fetch_assoc();
+        echo $row['list_data'];
+        return;
+    }
 }
 
 echo '{"success": false, "error": "Resource not found"}';
