b677f2a77af59ab26113470ff7b71edeb43f961c
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-06-03 11:31:15 +0200

    Sessions are saved to the database

--- api/login.php
+++ api/login.php
@@ -14,11 +14,22 @@ $requestContent = json_decode(file_get_contents('php://input'), true);
 $username = $requestContent['username'];
 $password = $requestContent['password'];
 
-$successful = ($username == 'admin') && ($password == 'admin');
-
-if($successful){
-    echo '{"success": true, "sessionId": "1234567890"}';
-} else {
+$db = new mysqli('localhost', 'root', '', 'photopos');
+$result = $db->query('SELECT * FROM users WHERE username = "'.$username.'" LIMIT 1');
+if($result->num_rows == 0){
     echo '{"success": false, "error": "Invalid login or password"}';
+    exit;
 }
+
+$row = $result->fetch_assoc();
+if($row['password_hash'] != hash('sha256', $password)){
+    echo '{"success": false, "error": "Invalid login or password"}';
+    exit;
+}
+
+$sessionId = uniqid();
+$db->query('INSERT INTO sessions (id, user_id) VALUES ("'.$sessionId.'", '.$row['id'].')');
+
+echo '{"success": true, "sessionId": "'.$sessionId.'"}';
+
 ?>
