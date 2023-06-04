53b4d0f1d44ae39f94e6bd26b84140a620253216
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-06-04 20:48:21 +0200

    Added register script

--- api/register.php
+++ api/register.php
@@ -17,21 +17,5 @@ $password = $requestContent['password'];
 $username = addslashes($username);
 
 $db = new mysqli('localhost', 'root', '', 'photopos');
-$result = $db->query('SELECT * FROM users WHERE username = "'.$username.'" LIMIT 1');
-if($result->num_rows == 0){
-    echo '{"success": false, "error": "Invalid login or password"}';
-    exit;
-}
-
-$row = $result->fetch_assoc();
-if($row['password_hash'] != hash('sha256', $password)){
-    echo '{"success": false, "error": "Invalid login or password"}';
-    exit;
-}
-
-$sessionId = uniqid();
-$db->query('INSERT INTO sessions (id, user_id) VALUES ("'.$sessionId.'", '.$row['id'].')');
-
-echo '{"success": true, "sessionId": "'.$sessionId.'"}';
-
+$db->query('INSERT INTO users (username, password_hash) VALUES ("'.$username.'", "'.hash('sha256', $password).'")');
 ?>
